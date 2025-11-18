d3960b5232173b5761940245bf227287eafd31ab
Matias Fernandez <fernandez.n.mati@gmail.com>
2025-11-18 13:24:22 +0900

    Add model validations and associations

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,3 +1,5 @@
 class Chat < ApplicationRecord
+  acts_as_chat
   belongs_to :component
+  has_many :messages, dependent: :destroy
 end
--- app/models/component.rb
+++ app/models/component.rb
@@ -1,3 +1,5 @@
 class Component < ApplicationRecord
   belongs_to :project
+  has_one :chat, dependent: :destroy
+  validates :name, presence: true, uniqueness: {scope: :project_id}
 end
