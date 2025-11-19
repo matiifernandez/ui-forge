597a30548d902ba93968f09d0e5dcf67781284cc
Lorenzo Panneman <unframed3@gmail.com>
2025-11-19 11:43:33 +0900

    Working on new page

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,4 +1,7 @@
 class ComponentsController < ApplicationController
+
+  before_action :set_project, only: %i[new create]
+
   def index
     @project = Project.find(params[:project_id])
     @components = @project.components
@@ -7,5 +10,30 @@ class ComponentsController < ApplicationController
   def preview
     @component = Component.find(params[:id])
     render layout: false
+  end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @component = Component.new
+  end
+
+  def create
+    @project.user = current_user
+    @project = Project.find(params[:project_id])
+    @component = Component.new(component_params)
+    @component.project = @project
+    if @component.save
+      redirect_to project_component_path(@project, @component)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def component_params
+    params.require(:component).permit(:name, :html_code, :css_code)
   end
 end
